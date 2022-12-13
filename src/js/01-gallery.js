724aa1f0ddf673d4fcd23071d5321b8ec6899595
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2022-12-13 21:08:18 +0100

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,12 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const ul = document.createElement("ul");
+ul.classList.add("gallery__list");
+gallery.append(ul);
 
-console.log(galleryItems);
+const imageMarkup = galleryItems.map(
+  (e) =>
+    `<li><img class="img" src="${e.preview}" alt="${e.description}" /></li>`
+);
+ul.insertAdjacentHTML("beforeend", imageMarkup);
