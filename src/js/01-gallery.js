d79916c6f7015e0fecad15b258b01b85aaf1a816
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2022-12-15 19:12:00 +0100

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,20 +1,28 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
 const gallery = document.querySelector(".gallery");
+
 const ul = document.createElement("ul");
+
 ul.classList.add("gallery__list");
+
 gallery.append(ul);
 
 const imageMarkup = galleryItems.map(
   (e) =>
     `<li><img class="img" src="${e.preview}" alt="${e.description}" /></li>`
 );
+
 ul.insertAdjacentHTML("beforeend", imageMarkup);
 
-const instance = basicLightbox.create(
-  `<img src="${galleryItems.original}" alt="${galleryItems.description}"/>`
-);
 function modalWindow(e) {
+  e.preventDefault;
+  const img = document.querySelector(".img");
+  const largeImg = img.getAttribute("src");
+  console.log(largeImg);
+  const instance = basicLightbox.create(`<h1>Elo</h1>`);
+
   instance.show();
 }
 
